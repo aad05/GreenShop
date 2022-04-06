ffd9d2949c2aa1d8e2f26108a1d97a98d0e64909
AbduvoiotovAsadbek <abduvoitovasadbek3@gmail.com>
2022-04-06 15:56:23 +0500

    Use Params Added

--- src/root/index.js
+++ src/root/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { data } from '../utils/navbar';
 import Navbar from '../components/Navbar';
+import ParamsComponent from '../components/ParamsComponent';
 
 const Root = () => {
   return (
@@ -10,6 +11,9 @@ const Root = () => {
         {data?.map(({ pathname, id, Component }) => (
           <Route path={pathname} key={id} element={<Component />} />
         ))}
+      </Route>
+      <Route element={<Navbar />}>
+        <Route path='shop/:type/:id' element={<ParamsComponent />} />
       </Route>
     </Routes>
   );
