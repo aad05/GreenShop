40bb9fa36b83ed7e1a2b68f3a4c89d731f90dd2a
islomjon6975 <islomjon0919@mail.ru>
2022-04-15 13:12:01 +0500

    v/CouponApply branch  has been changed to v/product/1

---
+++ src/mock/product.js
@@ -0,0 +1,35 @@
+import ourblogs1 from '../assets/imgs/ourblogs-1.png'
+import ourblogs2 from '../assets/imgs/ourblogs-2.png'
+import ourblogs3 from '../assets/imgs/ourblogs-3.png'
+
+
+export const data = [
+    {
+        id: 1,
+        img: ourblogs1,
+        title: 'Barbeton Daisy',
+        desc: 'sku',
+        price: 238
+    },
+    {
+        id: 2,
+        img: ourblogs2,
+        title: 'Barbeton Daisy',
+        desc: 'sku',
+        price: 238
+    },
+    {
+        id: 3,
+        img: ourblogs3,
+        title: 'Barbeton Daisy',
+        desc: 'sku',
+        price: 238
+    },
+    {
+        id: 4,
+        img: ourblogs3,
+        title: 'Barbeton Daisy',
+        desc: 'sku',
+        price: 238
+    },
+]
