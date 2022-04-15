d5ccf63a553f592fb74e532c63d31beb76b46f94
AbduvoiotovAsadbek <abduvoitovasadbek3@gmail.com>
2022-04-15 17:10:44 +0500

    New User Type Added

--- src/components/Navbar/Register/index.jsx
+++ src/components/Navbar/Register/index.jsx
@@ -9,27 +9,27 @@ import Navigator from '../../../context/NavigateContext';
 
 const Register = () => {
   const [authData, setAuthedData] = useContext(AuthorizationData);
-  const [userName, setUserName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [phone, setPhone] = useState('');
+  const [data, setData] = useState({
+    userName: '',
+    email: '',
+    password: '',
+    firstName: '',
+    lastName: '',
+    phone: '',
+    role: 'user',
+    storeName: '',
+  });
   const [showSpinner, setShowSpinner] = useState(false);
   const [navigateTo] = useContext(Navigator);
   const antIcon = (
     <LoadingOutlined style={{ fontSize: 24, color: '#fff' }} spin />
   );
-  const data = {
-    userName,
-    email,
-    password,
-    firstName,
-    lastName,
-    phone,
-    role: 'user',
+
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
   };
   const onSubmit = (e) => {
+    const { email, password, firstName, lastName, phone, userName } = data;
     if (e.key === 'Enter' || e.keyCode === 13 || e.type === 'click') {
       if (email && password && firstName && lastName && phone && userName) {
         authData.registerUser(data, navigateTo);
@@ -40,10 +40,22 @@ const Register = () => {
           } else {
             setShowSpinner(false);
           }
+          setData({
+            userName: '',
+            email: '',
+            password: '',
+            firstName: '',
+            lastName: '',
+            phone: '',
+            role: 'user',
+            storeName: '',
+          });
         }, 2000);
       }
     }
   };
+  const { email, password, firstName, lastName, phone, userName, storeName } =
+    data;
   return (
     <>
       <Wrapper>
@@ -52,46 +64,66 @@ const Register = () => {
         </Wrapper.ModalParagraph>
         <Wrapper.LoginInput
           active={firstName.length > 0}
-          onChange={(e) => setFirstName(e.target.value)}
+          onChange={handleChange}
           placeholder='First Name'
           value={firstName}
           onKeyDown={onSubmit}
+          name='firstName'
         />
         <Wrapper.LoginInput
           active={lastName.length > 0}
-          onChange={(e) => setLastName(e.target.value)}
+          onChange={handleChange}
           placeholder='Last Name'
           value={lastName}
           onKeyDown={onSubmit}
+          name='lastName'
         />
         <Wrapper.LoginInput
           active={phone.length > 0}
-          onChange={(e) => setPhone(e.target.value)}
+          onChange={handleChange}
           placeholder='Phone'
           value={phone}
           onKeyDown={onSubmit}
+          name='phone'
         />
         <Wrapper.LoginInput
           active={userName.length > 0}
-          onChange={(e) => setUserName(e.target.value)}
+          onChange={handleChange}
           placeholder='Username'
           value={userName}
           onKeyDown={onSubmit}
+          name='userName'
         />
         <Wrapper.LoginInput
           active={email.length > 0}
-          onChange={(e) => setEmail(e.target.value)}
+          onChange={handleChange}
           placeholder='Email'
           value={email}
           onKeyDown={onSubmit}
+          name='email'
         />
         <Wrapper.LoginInput
           active={password.length > 0}
-          onChange={(e) => setPassword(e.target.value)}
+          onChange={handleChange}
           placeholder='Password'
           value={password}
           onKeyDown={onSubmit}
+          name='password'
         />
+        <Wrapper.Select name='role' onChange={handleChange}>
+          <Wrapper.Option value={'user'}>User</Wrapper.Option>
+          <Wrapper.Option value={'seller'}>Seller</Wrapper.Option>
+        </Wrapper.Select>
+        {data.role === 'seller' && (
+          <Wrapper.LoginInput
+            active={storeName.length > 0}
+            onChange={handleChange}
+            placeholder='Store Name'
+            value={storeName}
+            onKeyDown={onSubmit}
+            name='storeName'
+          />
+        )}
         <Wrapper.Button mt='27' onClick={onSubmit}>
           {showSpinner ? <Spin indicator={antIcon} /> : 'Register'}
         </Wrapper.Button>
